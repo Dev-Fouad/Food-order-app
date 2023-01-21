3cd89c37022deb184324c0a5fd2abd5c86b62af4
fouad ajayi <fouadajayi6@gmail.com>
2023-01-21 08:06:26 +0100

    wefgh

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
 import './index.css';
-import App from './App';
+import App from './App.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+
+ReactDOM.render(<App/>, document.getElementById('root'))
+

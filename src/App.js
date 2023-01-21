11856c44049963597a0ff8efa7b5f134d85e29ef
fouad ajayi <fouadajayi6@gmail.com>
2023-01-21 23:26:07 +0100

    cc

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import { Fragment } from 'react';
+import { Fragment , useState } from 'react';
 
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 
 function App() {
+  const [cartisShown, setCartIsShown]=useState(false)
+
   return (
     <Fragment>
-      <Cart />/
+      {cartisShown  && <Cart />}
       <Header />
       <main>
         <Meals />

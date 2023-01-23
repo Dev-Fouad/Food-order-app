e98cc069e3c50ca56d29a0a387def044f01b8a90
fouad ajayi <fouadajayi6@gmail.com>
2023-01-23 12:58:50 +0100

    done

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -16,7 +16,6 @@ const cartReducer = (state, action) => {
     );
     const existingCartItem = state.items[existingCartItemIndex];
     let updatedItems;
-    
 
     if (existingCartItem) {
       const updatedItem = {
@@ -34,7 +33,7 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount,
     };
   }
-  
+
   if (action.type === 'REMOVE') {
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.id
